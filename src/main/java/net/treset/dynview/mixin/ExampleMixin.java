36bb20db6163e17f1e23ea5d1a591058a1a5ecac
hanneshekl@gmail.com <hanneshekl@gmail.com>
2022-07-23 13:29:23 +0200

    Changed mod properties

--- src/main/java/net/treset/dynview/mixin/ExampleMixin.java
+++ src/main/java/net/treset/dynview/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.treset.dynview.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.treset.dynview.DynViewMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		DynViewMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }

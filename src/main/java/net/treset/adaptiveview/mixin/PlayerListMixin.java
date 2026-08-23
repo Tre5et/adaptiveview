329bce882db4138e93976c5a123b13b89b14b407
ThatCuteOne <gmascuto@gmail.com>
2026-08-24 01:06:31 +0200

    style: cleanup unused imports

--- src/main/java/net/treset/adaptiveview/mixin/PlayerListMixin.java
+++ src/main/java/net/treset/adaptiveview/mixin/PlayerListMixin.java
@@ -1,23 +1,14 @@
 package net.treset.adaptiveview.mixin;
 
 import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientboundSetChunkCacheRadiusPacket;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.players.PlayerList;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.lang.annotation.Target;
 
 @Mixin(PlayerList.class)
 public abstract class PlayerListMixin {
-    // we just prevent the ClientboundSetChunkCacheRadiusPacket from being send to the players
+    // we just prevent the ClientboundSetChunkCacheRadiusPacket from being sent to the players
     @Redirect(method = "setViewDistance",
             at = @At(
                     value = "INVOKE",
